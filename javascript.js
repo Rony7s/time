07eb28ad8ea1c90a844ae8f717633e504f20b537
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2023-12-03 04:36:16 +0600

    Add files via upload
    after sir feedback

--- javascript.js
+++ javascript.js
@@ -35,3 +35,44 @@ function startCountdown() {
         }
     }, 1000);
 }
+
+//hide show
+
+let x = 0;
+let menu_item = document.getElementById("menu_item");
+let a = document.getElementById("timeSetting");
+function showMenu(){
+    x = x+1;
+    if(x%2==0){
+        a.style.marginLeft = "-500px";
+        menu_item.innerText ="Show Settings"
+    }else{
+        a.style.marginLeft = "50px";
+        menu_item.innerText ="Hide Settings"
+    }
+
+}
+ 
+//watchLiveCount
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var meridiem = hours >= 12 ? 'PM' : 'AM';
+
+    // Convert hours to 12-hour format
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    // Display the formatted time
+    var formattedTime = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
+    document.getElementById('watchLiveCount').innerHTML = formattedTime;
+  }
+
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+  updateClock();
